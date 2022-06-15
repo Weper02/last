d2dd44d3c0d7bd4ee6dd23c016b53dbb1ce5ad54
Weper02 <101238593+Weper02@users.noreply.github.com>
2022-06-15 13:07:58 +0800

    Add files via upload

---
+++ GeometricObject.h
@@ -0,0 +1,13 @@
+#ifndef GeometricObject_h
+#define GeometricObject_h
+#include<string>
+using namespace std;
+class GeometricObject {
+private:
+	string color;
+public:
+	void setColor(string);
+	string getColor();
+};
+#endif // !GeometricObject_h
+
